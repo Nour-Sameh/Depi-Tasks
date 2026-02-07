2f78adf219140553f028a4c15e434a1b037c6d3e
Nour Sameh <nono5noor4@gmail.com>
2026-02-07 19:02:58 +0200

    Add files via upload

---
+++ Session 3 Assignment 2/SavingAccount.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Session_3_2
+{
+    internal class SavingAccount : BankAccount
+    {
+        public decimal InterestRate { get; set; }
+
+        public SavingAccount(string fullName, string nationalID, string phoneNumber, string address, decimal balance,decimal interestRate)
+            :base(fullName, nationalID, phoneNumber, address, balance)
+        {
+            InterestRate = interestRate;
+        }
+        public override void ShowAccountDetails()
+        {
+            base.ShowAccountDetails();
+            Console.WriteLine($"InterestRate : {InterestRate}%");
+
+        }
+        public override decimal CalculateInterest()
+        {
+            return Balance * InterestRate / 100; 
+        }
+    }
+}
